631c6724c57f8569aeafb9e5ac58f2a3ece4e1d8
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2024-09-25 19:14:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function lifeInWeeks(age) {   
+    
+    var currentAge = prompt("Enter your current age");
+    var RemainingLifeInYears = 90-currentAge;
+    var days = RemainingLifeInYears*365;
+    var weeks = RemainingLifeInYears*52;
+    var months = RemainingLifeInYears*12;
+    console.log("You have "+days+" days, "+weeks+" weeks, and "+months+" months left..");
+
+}
+lifeInWeeks();
